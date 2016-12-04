e34ab4333a6017884f6560fe8783a6677158a4a0
Giovanni Lopez Celaya <giovanni.lopez3523@gmail.com>
2016-12-04 16:46:45 -0700

    Add files via upload

---
+++ scripts/shaders/fadeToBlack.glsl
@@ -0,0 +1,20 @@
+vec4 col;
+extern number percentage;
+number bright;
+vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords)
+{
+	col = texture2D(texture, texture_coords);
+	if (percentage < 50)
+	{
+		bright = 1;
+		//2-percentage/12.5;
+	} else {
+		bright = (percentage-75)/25;
+	}
+	
+	bright = min(bright,1);
+	
+	bright = - bright*bright + 2*bright;
+	
+	return vec4(col.r*bright, col.g*bright, col.b*bright, 1);
+}
